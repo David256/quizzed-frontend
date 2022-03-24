82bbca3f30c13802d57c40cfbc98aa62037704e5
David256 <wasterdavid@gmail.com>
2022-03-24 13:36:28 -0500

    Install eslint and config all

---
+++ src/app/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import App from './App';
+
+function Index() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+export default Index;
