24e65175c670a67671e7cf31d937a815fcd8ae36
David256 <wasterdavid@gmail.com>
2022-03-24 16:09:05 -0500

    Add QuizPage

---
+++ src/pages/QuizPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function QuizPage() {
+  return (
+    <div>
+      <h1>quiz</h1>
+      <button type="button">next</button>
+    </div>
+  );
+}
+
+export default QuizPage;
