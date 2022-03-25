6b9a477043a7d5d72b9ed1586a7fa45497964a88
David256 <wasterdavid@gmail.com>
2022-03-25 03:27:27 -0500

    Add style to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../helpers/routes';
+import './HomePage.scss';
 
 function HomePage() {
   return (
-    <div>
-      <h1>welcome to the trivia challenge</h1>
+    <div className="home-container">
+      <h1>Welcome to the trivia challenge</h1>
 
       <p>
-        you will be presented with 10 true or false question.
+        You will be presented with 10 true or false question.
       </p>
 
       <p>
-        can you score 100%?
+        Can you score 100%?
       </p>
 
       <Link to={routes.preface}>
-        <button type="button">begin</button>
+        <button className="home-button" type="button">begin</button>
       </Link>
     </div>
   );
