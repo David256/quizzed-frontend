b447d94ba9635955ad720785a124a7e460445275
David256 <wasterdavid@gmail.com>
2022-03-24 20:39:40 -0500

    Set all routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import PrefacePage from '../pages/PrefacePage';
+import QuizPage from '../pages/QuizPage';
+import ResultPage from '../pages/ResultPage';
+
+import routes from '../helpers/routes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hi!
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path={routes.home} element={<HomePage />} />
+        <Route exact path={routes.preface} element={<PrefacePage />} />
+        <Route exact path={routes.quiz} element={<QuizPage />} />
+        <Route exact path={routes.result} element={<ResultPage results={[]} />} />
+      </Routes>
+    </Router>
   );
 }
 
