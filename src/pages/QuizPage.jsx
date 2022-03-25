59c236c591c71db683ac52b2197765fcc4ac1452
David256 <wasterdavid@gmail.com>
2022-03-24 23:36:29 -0500

    Update QuizPage to show questions and get answers

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,10 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import log from 'loglevel';
+import useSession from '../session/useSession';
+import Option from '../components/Option';
 
 function QuizPage() {
+  const {
+    quizName,
+    answers,
+    setAnswers,
+    quiz,
+    requestQuiz,
+    sendAnswers,
+  } = useSession();
+
+  const [index, setIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+
+  /**
+   * Get next question and save in `currentQuestion`
+   */
+  const prepareNextQuestion = () => {
+    const question = quiz.questions[index];
+    log.debug(question);
+    setCurrentQuestion(question);
+  };
+
+  // Request the quiz
+  useEffect(() => {
+    requestQuiz();
+  }, []);
+
+  // When  we have quiz, start questioning
+  useEffect(() => {
+    if (quiz) {
+      log.info(quiz);
+      prepareNextQuestion();
+      setIndex(index + 1);
+    }
+  }, [quiz]);
+
+  const onAnswer = (response) => {
+    // Save the answer
+    log.info(`response ${response}`);
+
+    const newAnswer = {
+      questionId: currentQuestion.questionId,
+      response,
+    };
+
+    log.debug('save answers');
+    log.debug(newAnswer);
+
+    setAnswers([...answers, newAnswer]);
+
+    if (index < quiz.questions.length) {
+      log.debug('will prepare next quesstion');
+      setIndex(index + 1);
+      prepareNextQuestion();
+    } else {
+      log.debug('last question');
+      // Send responses (aka answers)
+      log.info(answers);
+      sendAnswers();
+    }
+  };
+
   return (
     <div>
-      <h1>quiz</h1>
-      <button type="button">next</button>
+      <h1>
+        quiz
+        {' '}
+        {quizName}
+      </h1>
+      <div>
+        {currentQuestion && (
+          <>
+            <strong>{currentQuestion.question}</strong>
+            <Option isCorrect={false} onClick={onAnswer} />
+            <Option isCorrect onClick={onAnswer} />
+          </>
+        )}
+      </div>
     </div>
   );
 }
