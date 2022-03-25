ee94a66b4280519db9b88d0e1bce4378d6635565
David256 <wasterdavid@gmail.com>
2022-03-24 22:00:49 -0500

    Add some provider and another state to selection

--- src/session/SessionProvider.jsx
+++ src/session/SessionProvider.jsx
@@ -6,13 +6,32 @@ export const sessionContext = createContext();
 function SessionProvider({ children }) {
   const [quizName, setQuizName] = useState('');
   const [userEmail, setUserEmail] = useState('');
+  const [providerOptions, setProviderOptions] = useState([
+    {
+      value: 'local',
+      text: 'Local provider',
+    },
+    {
+      value: 'quizapi',
+      text: 'Quizapi.io provider',
+    },
+    {
+      value: 'opentdb',
+      text: 'opentdb.com provider',
+    },
+  ]);
+  const [selectedProvider, setSelectedProvider] = useState(null);
 
   // eslint-disable-next-line react/jsx-no-constructed-context-values
   const contextValue = {
     quizName,
-    userEmail,
     setQuizName,
+    userEmail,
     setUserEmail,
+    providerOptions,
+    setProviderOptions,
+    selectedProvider,
+    setSelectedProvider,
   };
 
   return (
